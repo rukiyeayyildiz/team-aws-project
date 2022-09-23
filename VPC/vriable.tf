2837fb71a3b09bae38ef3ca905e9e22a687dbd5a
Malik20A <106113996+Malik20A@users.noreply.github.com>
2022-09-23 17:45:21 -0500

    a

--- VPC/vriable.tf
+++ VPC/vriable.tf
@@ -1,35 +1,11 @@
-variable "vpc_name" {
-  type        = string
-  default     = "test"
-  description = "provide a VPC name"
-}
+variable "vpc_name" {}
 
-variable "region" {
-  type        = string
-  default     = "us-east-1"
-  description = "provide a region"
-}
+variable "region" {}
 
-variable "cidr" {
-  type        = string
-  default     = ""
-  description = "provide a cidr block"
-}
+variable "cidr" {}
 
-variable "tags" {
-  type        = map(any)
-  default     = {}
-  description = "provide a tag for VPC"
-}
+variable "tags" {}
 
-variable "private_subnets" {
-  type        = list(any)
-  default     = []
-  description = "provide a cidr block for private subnets"
-}
+variable "private_subnets" {}
 
-variable "public_subnets" {
-  type        = list(any)
-  default     = []
-  description = "provide a cidr block for public subnets"
-}
+variable "public_subnets" {}
