e82c483fa2a814a97759d0dedb897699b6adfa88
Malik20A <106113996+Malik20A@users.noreply.github.com>
2022-09-23 16:51:17 -0500

    added envs

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,21 +1,23 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  name                  = var.vpc_name
+  cidr                  = var.cidr
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs                   = [
+          "${var.region}a", 
+          "${var.region}b", 
+          "${var.region}c"
+    ]
+  
+  
+  private_subnets       = var.private_subnets
+  public_subnets        = var.public_subnets
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_nat_gateway    = false
+  enable_vpn_gateway    = false
+  enable_dns_hostnames  = true
+  enable_dns_support    = true
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-    Dept = "IT"
-  }
+  tags                  = var.tags
 }
---
+++ VPC/vriable.tf
@@ -0,0 +1,35 @@
+variable "vpc_name" {
+  type        = string
+  default     = ""
+  description = "provide a VPC name"
+}
+
+variable "region" {
+  type        = string
+  default     = ""
+  description = "provide a region"
+}
+
+variable "cidr" {
+  type        = string
+  default     = ""
+  description = "provide a cidr block"
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = ""
+  description = "provide a tag for VPC"
+}
+
+variable "private_subnets" {
+  type        = list(any)
+  default     = ""
+  description = "provide a cidr block for private subnets"
+}
+
+variable "public_subnets" {
+  type        = list(any)
+  default     = ""
+  description = "provide a cidr block for public subnets"
+}
